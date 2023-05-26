22f0ae8dacf8922ce27ab6bb5780aa72904832ce
Quentin <quentin.piot@perfmgr.fr>
2023-05-26 19:41:35 +0200

    rocket

--- index.js
+++ index.js
@@ -36,5 +36,7 @@ app.all("*", (req, res) => {
 });
 
 app.listen(process.env.PORT, () => {
-  console.log("Server Started 🚀🚀🚀🚀🚀🚀");
+  console.log(
+    "Server Started 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀"
+  );
 });
